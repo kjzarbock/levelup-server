4b910bfb99cd2f0fc681ddf7ea4bf274c302d0ab
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-11 09:08:22 -0500

    8/14 deleted user.py

--- levelupapi/models/user.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    gamer = models.OneToOneField("Gamer", on_delete=models.CASCADE, null=True, blank=True)
-    full_name = models.CharField(max_length=100)
-    email = models.EmailField()
