d52bb9db39e15237dc31f216495c07ca76df6e5b
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-11 14:11:33 -0500

    8/15 fixed migrations

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Game(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=60)
     creator = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name="created_games")
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name="games")
