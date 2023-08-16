9d7e4aeea607c73016bb5ea7ce95391bb9378664
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-16 17:16:51 -0500

    8/18 1404, added gamer view to capture users on client side

---
+++ levelupapi/views/gamer_view.py
@@ -0,0 +1,36 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Gamer
+
+
+class GamerView(ViewSet):
+    """Level up game types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game type
+        Returns:
+            Response -- JSON serialized game type
+        """
+        gamer = Gamer.objects.get(pk=pk)
+        serializer = GamerSerializer(gamer)
+        return Response(serializer.data)
+
+    def list(self, request):
+        """Handle GET requests to get all game types
+
+        Returns:
+            Response -- JSON serialized list of game types
+        """
+        gamers = Gamer.objects.all()
+        serializer = GamerSerializer(gamers, many=True)
+        return Response(serializer.data)
+
+class GamerSerializer(serializers.ModelSerializer):
+    """JSON serializer for customers"""
+
+    class Meta:
+        model = Gamer
+        fields = ('id', 'bio', 'events')
