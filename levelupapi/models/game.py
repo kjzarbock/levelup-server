de28515a425f8b1b7fd6784a77dda9facd83fc30
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-15 19:03:46 -0500

    8/16 1107, fixed game model and fixture

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -4,3 +4,6 @@ class Game(models.Model):
     title = models.CharField(max_length=60)
     creator = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name="created_games")
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name="games")
+    maker = models.CharField(max_length=25)
+    number_of_players = models.IntegerField()
+    skill_level = models.CharField(max_length=10)
