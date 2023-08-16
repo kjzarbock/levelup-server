aca7edacf291469b5e088ee2453d3be8079b3506
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-16 17:37:23 -0500

    8/18 1423, added PUT for games--Next: Do for events

--- levelupapi/views/game_view.py
+++ levelupapi/views/game_view.py
@@ -42,6 +42,20 @@ class GameView(ViewSet):
         )
         serializer = GameSerializer(game)
         return Response(serializer.data)
+    
+    def update(self, request, pk):
+        game = Game.objects.get(pk=pk)
+        game.title = request.data["title"]
+        game.maker = request.data["maker"]
+        game.number_of_players = request.data["number_of_players"]
+        game.skill_level = request.data["skill_level"]
+
+        game_type = GameType.objects.get(pk=request.data["game_type"])
+        game.game_type = game_type
+        game.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
 
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for customers"""
